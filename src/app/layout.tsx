1e924ca6a610b026776184818dcdeda1d19362f6
nathanbrn <breno_nathanael@hotmail.com>
2024-05-02 11:15:27 -0300

    Implementação do react query para http state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,26 @@
+'use client'
+
 import '@/style/global.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const queryClient = new QueryClient();
+
   return (
     <html lang="pt-BR">
       <head>
         <title>MKS Sistemas</title>
       </head>
-      <body>{children}</body>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,32 @@ import { useEffect, useState } from "react";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from "@mui/material";
-import { TransitionProps } from '@mui/material/transitions';
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+
   const [open, setOpen] = useState<boolean>(false);
-  const [products, setProducts] = useState<Product[] | null>(null);
+  const { data: products, isLoading } = useQuery({
+    queryKey: ['products'],
+    queryFn: getProducts,
+  });
+  // const [products, setProducts] = useState<Product[] | null>(null);
   const [productsCart, setProductsCart] = useState<Product[] | null>(null);
   const [total, setTotal] = useState<number>(0);
   const [openAlert, setOpenAlert] = useState<boolean>(false);
+
+  async function getProducts() {
+    try {
+      const response = await axios.get('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=10&sortBy=id&orderBy=ASC');
+      return response.data.products;
+    } catch (error) {
+      console.error('Erro ao buscar os produtos:', error);
+      throw new Error('Erro ao buscar os produtos');
+    }
+  }
+
+
 
   function openDrawer() {
     setOpen(true);
@@ -38,20 +55,10 @@ export default function Home() {
     setOpen(false);
   }
 
-  useEffect(() => {
-    (async () => {
-      await axios.get('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=10&sortBy=id&orderBy=ASC').then((product) => {
-        setProducts(product.data.products);
-      }).catch((error) => {
-        console.log(error.message);
-      })
-    })();
-  }, []);
-
   return (
     <>
       <Header ProductsInCart={productsCart} openDrawer={openDrawer} />
-      <Main total={total} setTotal={setTotal} products={products} setProductsCart={setProductsCart} productsCart={productsCart} />
+      <Main total={total} setTotal={setTotal} products={products ?? []} setProductsCart={setProductsCart} productsCart={productsCart} />
       <DrawerComponent showAlert={showAlert} total={total} setTotal={setTotal} open={open} closeDrawer={closeDrawer} productsCart={productsCart} setProductsCart={setProductsCart} />
       <ToastContainer />
       <Dialog
@@ -62,19 +69,23 @@ export default function Home() {
       >
         <DialogTitle>Compra Finalizada</DialogTitle>
         <DialogContent>
+          <br />
           <DialogContentText id="alert-dialog-slide-description">
-            <br />
-            <p>Obrigado por comprar!</p>
-            <br />
-            <p>Sua compra deu um total de: R${total.toLocaleString('BR')}</p>
-            <br />
-            <p>Os itens comprados foram:</p>
-            <ul>
-              {productsCart?.map(({ name, id }: Product, index) => (
-                <li style={{ marginLeft: 8}} key={id}>{index+1} - {name}</li>
-              ))}
-            </ul>
+            Obrigado por comprar!
           </DialogContentText>
+          <br />
+          <DialogContentText>
+            Sua compra deu um total de: R${total.toLocaleString('BR')}
+          </DialogContentText>
+          <br />
+          <DialogContentText>
+            Os itens comprados foram:
+          </DialogContentText>
+          <ul>
+            {productsCart?.map(({ name, id }: Product, index) => (
+              <li style={{ marginLeft: 8 }} key={id}>{index + 1} - {name}</li>
+            ))}
+          </ul>
         </DialogContent>
         <DialogActions>
           <Button onClick={hideAlert}>Fechar</Button>
